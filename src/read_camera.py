65e12dfde24fd782b2ea05cd73a9d77f8c397b8e
Oleksandra Yashchenko <oleksandra.yash4enk0@gmail.com>
2025-11-16 17:23:01 +0200

    Camera auto exposition and auto white balance + save pictures to files

--- src/read_camera.py
+++ src/read_camera.py
@@ -1,11 +1,56 @@
-from picamera2 import Picamera2
+import os, threading
 import time
+from datetime import datetime
+from picamera2 import Picamera2
 
-picam2 = Picamera2()
+class Camera:
+    def __init__(self):
+        self.camera = None
+        self.photo_dir = 'images'
+        self.photo_path = None
 
-camera_config = picam2.create_preview_configuration()
-picam2.configure(camera_config)
-picam2.start()
+        os.makedirs(self.photo_dir, exist_ok=True)
 
-time.sleep(2)
-picam2.capture_file('test_photo.jpg')
+    def capture_photo(self, exit_event: threading.Event):
+        while not exit_event.is_set():
+            try:
+                if not self.camera:
+                    self.camera = Picamera2()
+                    camera_config = self.camera.create_still_configuration(
+                        main={'size': (1920, 1080)},
+                        controls={
+                            'AeEnable': True,
+                            'AwbEnable': True
+                        }
+                    )
+                    self.camera.configure(camera_config)
+                    
+                self.camera.start()
+                time.sleep(0.015)
+                print("[Camera] Initialized")
+
+                timestamp = datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
+                self.photo_path = os.path.join(self.photo_dir, f"plant_{timestamp}.jpg")
+                self.camera.capture_file(self.photo_path)
+                print('Taken!')
+
+                self.camera.stop()
+                time.sleep(1)
+
+            except Exception as e:
+                print(e)
+                return None
+
+exit_event = threading.Event()
+cam = Camera()
+cam_thread = threading.Thread(target=cam.capture_photo, args=(exit_event,), daemon=True)
+cam_thread.start()
+
+try:
+    while True:
+        print(cam.photo_path)
+        time.sleep(1)
+except KeyboardInterrupt:
+    exit_event.set()
+    cam.camera.stop()
+    cam_thread.join(timeout=1)
